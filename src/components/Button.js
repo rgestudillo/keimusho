e4d4788a039ffa2b29ab1f8f3a96e68ff344a195
rgestudillo <90782009+rgestudillo@users.noreply.github.com>
2022-04-19 01:05:47 +0800

    Update Button.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -40,9 +40,9 @@ const Btn = styled.button`
 
 const Button = ({ text, link }) => {
   return (
-      <a href={link} aria-label={text} target="_blank" rel="noreferrer" >
-<Btn>{text}</Btn>
-</a>
+ <a href={link} aria-label={text} target="_blank" rel="noreferrer" >
+    <Btn>{text}</Btn>
+    </a>
   );
 };
 
