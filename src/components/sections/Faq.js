0a2eb6d928023f4db75485deb8e764c466d4e48a
Ket <keathian.lavador@gmail.com>
2022-04-19 17:07:04 +0800

    changes on faq.js

--- src/components/sections/Faq.js
+++ src/components/sections/Faq.js
@@ -91,18 +91,18 @@ const Faq = () => {
       <Title>FAQ</Title>
       <Container>
         <Box>
-          <Accordion title="WHERE CAN I VIEW MY NFTS?">
+          <Accordion title="WHAT IS THE SUPPLY?">
             Once minted or bought simply connect to your OpenSea account to view
             your NFTs.
           </Accordion>
-          <Accordion title="WHAT IS THE METAVERSE?">
+          <Accordion title="WHAT IS THE MINT PRICE?">
             A metaverse is a network of 3D virtual worlds focused on social
             connection. In futurism and science fiction, it is often described
             as a hypothetical iteration of the Internet as a single, universal
             virtual world that is facilitated by the use of virtual and
             augmented reality headsets.
           </Accordion>
-          <Accordion title="WHY DO WE NEED ROYALTIES?">
+          <Accordion title="WHAT IS THE MINT DATE?">
             The amount of royalties was fixed at 5% to finance the Weirdos
             Club's projects. We have the ambition to organize multiple events
             around the world in order to strengthen the community and build a
@@ -111,18 +111,18 @@ const Faq = () => {
           </Accordion>
         </Box>
         <Box>
-          <Accordion title="HOW CAN I USE MY NFT?">
+          <Accordion title="WHAT IS KEIMUSHO自由?">
             You will be able to use your NFT as an avatar in the Metaverse and
             our future video game. Holding also means that you are part of an
             exclusive network of investors and entrepreneurs.
           </Accordion>
-          <Accordion title="WHAT ARE THE WEIRDOS?">
+          <Accordion title="WHERE ARE WE LAUNCHING?">
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore
             deserunt consequatur quisquam maxime molestias dolores ipsum,
             exercitationem vel sint quidem aliquam modi quis impedit corporis
             unde inventore fugiat provident in.
           </Accordion>
-          <Accordion title="WHAT IS THE FUSION PROCESS?">
+          <Accordion title="HOW TO GET WHITELISTED?">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel enim
             veritatis iusto officia. Exercitationem, ducimus reiciendis. Rem,
             maxime, similique neque minus aliquam dolore doloremque laboriosam,
