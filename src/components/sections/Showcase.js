57c295636603fcda9539d3888ffc0c347a4076c5
Ket <keathian.lavador@gmail.com>
2022-04-19 16:38:56 +0800

    changes on showcase.js

--- src/components/sections/Showcase.js
+++ src/components/sections/Showcase.js
@@ -3,15 +3,15 @@ import styled from "styled-components";
 import { keyframes } from "styled-components";
 
 import img1 from "../../assets/Nfts/test.png";
-import img2 from "../../assets/Nfts/bighead-1.svg";
-import img3 from "../../assets/Nfts/bighead-2.svg";
-import img4 from "../../assets/Nfts/bighead-3.svg";
-import img5 from "../../assets/Nfts/bighead-4.svg";
-import img6 from "../../assets/Nfts/bighead-5.svg";
-import img7 from "../../assets/Nfts/bighead-6.svg";
-import img8 from "../../assets/Nfts/bighead-7.svg";
-import img9 from "../../assets/Nfts/bighead-8.svg";
-import img10 from "../../assets/Nfts/bighead-9.svg";
+import img2 from "../../assets/Nfts/test.png";
+import img3 from "../../assets/Nfts/test.png";
+import img4 from "../../assets/Nfts/test.png";
+import img5 from "../../assets/Nfts/test.png";
+import img6 from "../../assets/Nfts/test.png";
+import img7 from "../../assets/Nfts/test.png";
+import img8 from "../../assets/Nfts/test.png";
+import img9 from "../../assets/Nfts/test.png";
+import img10 from "../../assets/Nfts/test.png";
 import ETH from "../../assets/icons8-ethereum-48.png";
 
 const Section = styled.section`
