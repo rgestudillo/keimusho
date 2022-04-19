342ee09898dee1e39b7909ef61c265acc39566de
Ket <keathian.lavador@gmail.com>
2022-04-20 04:24:14 +0800

    changes in covervideo.js

--- src/components/CoverVideo.js
+++ src/components/CoverVideo.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import GIF from "../assets/Home Video.mp4";
+import GIF from "../assets/Coming_soon.gif";
 
 const VideoContainer = styled.div`
   width: 100%;
