66dde6e9678538db5114405f849325ab90a410ac
Ket <keathian.lavador@gmail.com>
2022-04-20 03:45:17 +0800

    changes in home.js

--- src/components/sections/Home.js
+++ src/components/sections/Home.js
@@ -47,58 +47,7 @@ const rotate = keyframes`
   100% {
     transform: rotate(1turn);
   }
-`;
 
-const Round = styled.div`
-  position: absolute;
-  bottom: 2rem;
-  right: 90%;
-  width: 6rem;
-  height: 6rem;
-  border: 1px solid ${(props) => props.theme.text};
-  border-radius: 50%;
-
-  img {
-    width: 100%;
-    height: auto;
-    animation: ${rotate} 6s linear infinite reverse;
-  }
-
-  @media (max-width: 64em) {
-    width: 4rem;
-    height: 4rem;
-    left: none;
-    right: 2rem;
-    bottom: 100%;
-  }
-
-  @media (max-width: 48em) {
-    right: 1rem;
-  }
-`;
-
-const Circle = styled.span`
-  width: 3rem;
-  height: 3rem;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border-radius: 50%;
-
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-
-  background-color: ${(props) => props.theme.text};
-  color: ${(props) => props.theme.body};
-  font-size: ${(props) => props.theme.fontxl};
-
-  @media (max-width: 64em) {
-    width: 2rem;
-    height: 2rem;
-    font-size: ${(props) => props.theme.fontlg};
-  }
 `;
 
 const Home = () => {
