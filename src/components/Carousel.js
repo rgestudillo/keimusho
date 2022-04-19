e13b3b6e3d2059f6e783b915931e1cdf80ef597a
Ket <keathian.lavador@gmail.com>
2022-04-19 16:35:04 +0800

    changes on carousel.js

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -11,15 +11,15 @@ import "swiper/css/navigation";
 import { Pagination, Navigation, Autoplay, EffectCards } from "swiper";
 
 import img1 from "../assets/Nfts/test.png";
-import img2 from "../assets/Nfts/bighead-1.svg";
-import img3 from "../assets/Nfts/bighead-2.svg";
-import img4 from "../assets/Nfts/bighead-3.svg";
-import img5 from "../assets/Nfts/bighead-4.svg";
-import img6 from "../assets/Nfts/bighead-5.svg";
-import img7 from "../assets/Nfts/bighead-6.svg";
-import img8 from "../assets/Nfts/bighead-7.svg";
-import img9 from "../assets/Nfts/bighead-8.svg";
-import img10 from "../assets/Nfts/bighead-9.svg";
+import img2 from "../assets/Nfts/test.png";
+import img3 from "../assets/Nfts/test.png";
+import img4 from "../assets/Nfts/test.png";
+import img5 from "../assets/Nfts/test.png";
+import img6 from "../assets/Nfts/test.png";
+import img7 from "../assets/Nfts/test.png";
+import img8 from "../assets/Nfts/test.png";
+import img9 from "../assets/Nfts/test.png";
+import img10 from "../assets/Nfts/test.png";
 
 import Arrow from "../assets/Arrow.svg";
 
