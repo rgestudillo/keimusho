467c90f8d78a0d7963d188cc882d85f27cad8f9d
Ket <keathian.lavador@gmail.com>
2022-04-19 16:36:51 +0800

    changes on showcase.js

--- src/components/sections/Showcase.js
+++ src/components/sections/Showcase.js
@@ -2,7 +2,7 @@ import React, { useRef } from "react";
 import styled from "styled-components";
 import { keyframes } from "styled-components";
 
-import img1 from "../../assets/Nfts/bighead.svg";
+import img1 from "../../assets/Nfts/test.png";
 import img2 from "../../assets/Nfts/bighead-1.svg";
 import img3 from "../../assets/Nfts/bighead-2.svg";
 import img4 from "../../assets/Nfts/bighead-3.svg";
